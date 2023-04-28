32d3006241f60ac9e8d7be32f816e4c5ae1df322
nnodim <nnodimobinna@gmail.com>
2023-04-28 01:39:42 +0100

    6. Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,3 +12,10 @@ def hash_password(password: str) -> bytes:
     password = password.encode()
     hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Check password is valid
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
