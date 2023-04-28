5b85987e5a001230078e88496e1c8839cee28537
nnodim <nnodimobinna@gmail.com>
2023-04-28 01:14:26 +0100

    1. Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -32,6 +32,9 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """
+        redact the message of LogRecord instance
+        """
         message = super(RedactingFormatter, self).format(record)
         return filter_datum(self.fields, self.REDACTION,
                             message, self.SEPARATOR)
