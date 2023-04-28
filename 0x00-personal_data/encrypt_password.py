050b7cd2e79d972a54e697f48dc588b0f004362f
nnodim <nnodimobinna@gmail.com>
2023-04-28 01:37:44 +0100

    5. Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Encrypt password
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Returns a hashed password
+    """
+    password = password.encode()
+    hashed_password = bcrypt.hashpw(b, bcrypt.gensalt())
+    return hashed_password
