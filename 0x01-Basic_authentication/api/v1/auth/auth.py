8cd11490ea87f9592810c7289ff27047d3db899e
nnodim <nnodimobinna@gmail.com>
2023-05-03 05:24:02 +0100

    4. Define which routes don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,20 @@ class Auth:
         """
         Determines whether a given path requires authentication or not
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for excluded_path in excluded_paths:
+            if excluded_path.startswith(path):
+                return False
+            elif path.startswith(excluded_path):
+                return False
+            elif excluded_path[-1] == "*":
+                if path.startswith(excluded_path[:-1]):
+                    return False
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
