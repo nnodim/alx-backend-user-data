3e9f85373bbbd0d99e0eae1a08dceaac383035ba
nnodim <nnodimobinna@gmail.com>
2023-05-07 03:46:42 +0100

    10. Sessions in database

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -2,10 +2,8 @@
 """
 Module for SessionDButh
 """
-from api.v1.auth.session_exp_auth import SessionExpAuth
+from .session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
-from uuid import uuid4
-from datetime import datetime, timedelta
 
 
 class SessionDBAuth(SessionExpAuth):
@@ -18,16 +16,16 @@ class SessionDBAuth(SessionExpAuth):
         Create a Session ID
         """
         session_id = super().create_session(user_id)
-        if session_id:
-            new_session = UserSession(
+        if not session_id:
+            return None
+        new_session = UserSession(
                 {
                     "user_id": user_id,
                     "session_id": session_id
                 }
             )
-            new_session.save()
-            return session_id
-        return None
+        new_session.save()
+        return session_id
 
     def user_id_for_session_id(self, session_id=None):
         """
@@ -44,11 +42,13 @@ class SessionDBAuth(SessionExpAuth):
         """
         Destroy user session
         """
+        if request is None:
+            return False
         session_id = self.session_cookie(request)
         if not session_id:
             return False
         user_session = UserSession.search({'session_id': session_id})
         if not user_session:
             return False
-        user_session.remove()
+        user_session[0].remove()
         return True
