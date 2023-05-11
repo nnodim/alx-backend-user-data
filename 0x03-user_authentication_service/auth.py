75f12600a0672b6138b0bdbc64fd11f6eae85950
nnodim <nnodimobinna@gmail.com>
2023-05-12 00:43:01 +0100

    8. Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -35,3 +35,14 @@ class Auth:
             hashed_password = _hash_password(password)
 
             return self._db.add_user(email, hashed_password)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Valid login
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        hashed_password = user.hashed_password
+        return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
