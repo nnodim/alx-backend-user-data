48cdcc18a675bfc9a1dddc2f8a0c3ff0ccbc1320
nnodim <nnodimobinna@gmail.com>
2023-05-12 02:07:54 +0100

    19. Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -95,5 +95,20 @@ def get_reset_password_token() -> str:
     })
 
 
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password():
+    """
+    update password
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        Auth.update_password(reset_token, new_password)
+    except ValueError:
+        return jsonify({'error': 'Invalid reset token'}), 403
+    return jsonify({'email': email, 'message': 'Password updated'}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
