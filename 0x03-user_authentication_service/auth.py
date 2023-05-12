5f7c1fe3a4d81f09f99d2f0fc470bc5b6402ec60
nnodim <nnodimobinna@gmail.com>
2023-05-12 01:06:37 +0100

    13. Destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -81,3 +81,15 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        destroy session
+        """
+        try:
+            user = self._db.find_user_by(user_id=user_id)
+        except NoResultFound:
+            return None
+
+        user.session_id = None
+        self._db.commit()
